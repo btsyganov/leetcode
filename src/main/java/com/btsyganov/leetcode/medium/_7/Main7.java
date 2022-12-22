38bddc1e36e4b581ef677a29b38d929328ad7b9a
Boris Tsyganov <B.Tsyganov@otpbank.ru>
2022-12-22 16:44:01 +0300

    7. Reverse Integer universal solution

--- src/main/java/com/btsyganov/leetcode/medium/_7/Main7.java
+++ src/main/java/com/btsyganov/leetcode/medium/_7/Main7.java
@@ -8,9 +8,13 @@ public class Main7 {
 
   public static void main(String[] args) {
     assert 321 == main7.reverse(123);
+    assert 321 == main7.reverse2(123);
     assert 21 == main7.reverse(120);
+    assert 21 == main7.reverse2(120);
     assert -321 == main7.reverse(-123);
+    assert -321 == main7.reverse2(-123);
     assert 0 == main7.reverse(-2000000009);
+    assert 0 == main7.reverse2(-2000000009);
   }
 
   /**
@@ -34,6 +38,27 @@ public class Main7 {
     }
 
     return (int) (isNegative ? -1 * base : base);
+  }
+
+  /**
+   * Universal implementation
+   */
+  public int reverse2(int x) {
+    int res = 0;
+    while (true) {
+      int body = x / 10;
+      int digit = x % 10;
+      x = body;
+      if ((res < Integer.MIN_VALUE / 10 || res > Integer.MAX_VALUE / 10)
+          || res == Integer.MIN_VALUE / 10 && x < -8 || res == Integer.MAX_VALUE / 10 && x > 7
+      ) {
+        return 0;
+      }
+      res = res * 10 + digit;
+      if (body == 0) {
+        return res;
+      }
+    }
   }
 
 }
